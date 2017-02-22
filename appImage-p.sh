17bb3a6b8da6f5a1b6b3277750de7218a06fac09
develar <develar@gmail.com>
2017-02-22 21:59:42 +0100

    appImage — bundle required libs for Fedora

--- appImage-p.sh
+++ appImage-p.sh
@@ -6,6 +6,8 @@ cd /tmp/appimage
 
 mkdir packages
 
+# required for ElementaryOS
+
 curl http://mirrors.kernel.org/ubuntu/pool/main/g/gconf/libgconf-2-4_3.2.6-0ubuntu2_amd64.deb -o f.deb
 dpkg-deb -R f.deb ef
 mv ef/usr/lib/x86_64-linux-gnu/libgconf-2.so.4.1.5 packages/libgconf-2.so.4
@@ -22,3 +24,23 @@ curl http://mirrors.kernel.org/ubuntu/pool/main/liba/libappindicator/libappindic
 dpkg-deb -R f.deb ef
 mv ef/usr/lib/x86_64-linux-gnu/libappindicator.so.1.0.0 packages/libappindicator.so.1
 
+# just to be sure
+
+rm -rf ef
+
+curl http://security.ubuntu.com/ubuntu/pool/main/n/nss/libnss3_3.26.2-0ubuntu0.14.04.3_amd64.deb -o f.deb
+dpkg-deb -R f.deb ef
+mv ef/usr/lib/x86_64-linux-gnu/ packages/
+
+rm -rf ef
+
+curl http://mirrors.kernel.org/ubuntu/pool/main/libx/libxtst/libxtst6_1.2.2-1_amd64.deb -o f.deb
+dpkg-deb -R f.deb ef
+mv ef/usr/lib/x86_64-linux-gnu/libXtst.so.6.1.0 packages/libXtst.so.6
+
+rm -rf ef
+
+# required for fedora
+curl http://mirrors.kernel.org/ubuntu/pool/main/libx/libxss/libxss1_1.2.2-1_amd64.deb -o f.deb
+dpkg-deb -R f.deb ef
+mv ef/usr/lib/x86_64-linux-gnu/libXss.so.1.0.0 packages/libXss.so.1
