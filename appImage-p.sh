358006e2483c604b96336490181dcd9a3219f19b
develar <develar@gmail.com>
2017-03-10 18:19:16 +0100

    fix: remove nss (leads to conflicts and actually required only for fedora)

--- appImage-p.sh
+++ appImage-p.sh
@@ -20,27 +20,21 @@ mv ef/usr/lib/x86_64-linux-gnu/libnotify.so.4.0.0 packages/libnotify.so.4
 
 rm -rf ef
 
+# libappindicator1
 curl http://mirrors.kernel.org/ubuntu/pool/main/liba/libappindicator/libappindicator1_12.10.1+13.10.20130920-0ubuntu4_amd64.deb -o f.deb
 dpkg-deb -R f.deb ef
-mv ef/usr/lib/x86_64-linux-gnu/libappindicator.so.1.0.0 packages/libappindicator.so.1
-
-# just to be sure
+mv ef/usr/lib/libappindicator.so.1.0.0 packages/libappindicator.so.1
 
 rm -rf ef
 
-curl http://security.ubuntu.com/ubuntu/pool/main/n/nss/libnss3_3.26.2-0ubuntu0.14.04.3_amd64.deb -o f.deb
-dpkg-deb -R f.deb ef
-mv ef/usr/lib/x86_64-linux-gnu/ packages/
-
-rm -rf ef
-
+# libxtst6
 curl http://mirrors.kernel.org/ubuntu/pool/main/libx/libxtst/libxtst6_1.2.2-1_amd64.deb -o f.deb
 dpkg-deb -R f.deb ef
 mv ef/usr/lib/x86_64-linux-gnu/libXtst.so.6.1.0 packages/libXtst.so.6
 
 rm -rf ef
 
-# required for fedora
+# libxss1, required for fedora
 curl http://mirrors.kernel.org/ubuntu/pool/main/libx/libxss/libxss1_1.2.2-1_amd64.deb -o f.deb
 dpkg-deb -R f.deb ef
 mv ef/usr/lib/x86_64-linux-gnu/libXss.so.1.0.0 packages/libXss.so.1
