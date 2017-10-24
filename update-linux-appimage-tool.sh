7cb77289e366b5b982b199467236298f5ea6ada8
develar <develar@gmail.com>
2017-10-24 08:20:31 +0200

    feat(AppImage): support image 2 format

---
+++ update-linux-appimage-tool.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd /tmp
+rm -rf appimagetool
+mkdir appimagetool
+cd appimagetool
+curl -L https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-i686.AppImage -o tool
+chmod +x tool
+docker run --rm -v ${PWD}:/project i386/ubuntu:xenial /bin/bash -c "cd /project && ./tool --appimage-extract"
+cp squashfs-root/usr/bin/* $BASEDIR/AppImage/linux-ia32/
+
+rm -rf appimagetool
+mkdir appimagetool
+cd appimagetool
+curl -L https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -o tool
+chmod +x tool
+docker run --rm -v ${PWD}:/project buildpack-deps:xenial-curl /bin/bash -c "cd /project && ./tool --appimage-extract"
+cp squashfs-root/usr/bin/* $BASEDIR/AppImage/linux-x64/
