1d15f0b4c62720c5b18c0865416e45ee944e9465
beyondkmp <beyondkmp@gmail.com>
2023-09-12 15:57:06 +0800

    fix compile problems (#51)
    Co-authored-by: payne.fu <payne.fu@ringcentral.com>

--- zstd-linux.sh
+++ zstd-linux.sh
@@ -17,7 +17,7 @@ docker run --cidfile="$cidFile" buildpack-deps:bionic bash -c \
 'git clone --depth 1 --branch v1.5.0 https://github.com/facebook/zstd.git && cd zstd && make -j5 install && cd .. &&
  git clone --depth 1 --branch 4.5 https://github.com/plougher/squashfs-tools && cd squashfs-tools/squashfs-tools &&
  apt-get update -y && apt-get install -y liblzo2-dev && make -j5 XZ_SUPPORT=1 LZO_SUPPORT=1 ZSTD_SUPPORT=1 GZIP_SUPPORT=0 COMP_DEFAULT=zstd install &&
- cp /usr/local/bin/mksquashfs /tmp/mksquashfs-64 &&
+ cp /usr/local/bin/mksquashfs /tmp/mksquashfs-64
  '
 
 containerId=$(cat "$cidFile")
