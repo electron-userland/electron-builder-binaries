074a6e8e6e8432bc92e82e408ec2dde769161dd9
Mike Maietta <mmaietta@outlook.com>
2025-04-28 13:06:18 -0700

    cleanup

--- scripts/changeset-version.js
+++ scripts/changeset-version.js
@@ -1,12 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const { execSync } = require("child_process");
-const os = require("os");
 
-// const { version } = require("../package.json")
-// const { GITHUB_REF, GITHUB_SHA } = process.env
-
-// changeset status outputs at __dirname even if we set absolute output path
+// changeset status expects relative __dirname even if we set absolute output path
 const changesetJsonPath = "changeset-status.json";
 
 execSync(`pnpm changeset status --output ${changesetJsonPath}`);
