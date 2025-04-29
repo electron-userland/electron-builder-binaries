9dfbe8590aa3775119699345ac08271cd2f313cf
Mike Maietta <mmaietta@outlook.com>
2025-04-28 17:46:43 -0700

    add release script

--- scripts/changeset-version.js
+++ scripts/changeset-version.js
@@ -21,32 +21,17 @@ const packageMap = {
   "win-codesign": ["win-codesign-2.6.0.7z"],
   wine: ["wine-4.0.1-mac.7z"],
   wix: ["wix-4.0.0.5512.2.7z"],
-  zstd: [
-    "zstd-v1.5.5-linux-x64.7z",
-    "zstd-v1.5.5-mac.7z",
-    "zstd-v1.5.5-win-ia32.7z",
-    "zstd-v1.5.5-win-x64.7z",
-  ],
-  fpm: [
-    "fpm-1.9.3-2.3.1-linux-x86_64.7z",
-    "fpm-1.9.3-2.3.1-linux-x86.7z",
-    "fpm-1.9.3-20150715-2.2.2-mac.7z",
-  ],
+  zstd: ["zstd-v1.5.5-linux-x64.7z", "zstd-v1.5.5-mac.7z", "zstd-v1.5.5-win-ia32.7z", "zstd-v1.5.5-win-x64.7z"],
+  fpm: ["fpm-1.9.3-2.3.1-linux-x86_64.7z", "fpm-1.9.3-2.3.1-linux-x86.7z", "fpm-1.9.3-20150715-2.2.2-mac.7z"],
   "linux-tools": ["linux-tools-mac-10.12.4.7z"],
-  "snap-template": [
-    "snap-template-electron-4.0-1-amd64.tar.7z",
-    "snap-template-electron-4.0-1-armhf.tar.7z",
-    "snap-template-electron-4.0-2-amd64.tar.7z",
-    "snap-template-electron-4.0.tar.7z",
-  ],
+  "snap-template": ["snap-template-electron-4.0-1-amd64.tar.7z", "snap-template-electron-4.0-1-armhf.tar.7z", "snap-template-electron-4.0-2-amd64.tar.7z", "snap-template-electron-4.0.tar.7z"],
 };
 
 releases.forEach((release) => {
   const { name } = release;
   const artifactsToUpload = packageMap[name];
   if (!artifactsToUpload) {
-    console.warn(`No artifacts found for ${name}`);
-    return;
+    throw new Error(`No artifacts found for ${name}`);
   }
   console.log(`Committing artifacts for ${name}...`);
   artifactsToUpload.forEach((artifact) => {
